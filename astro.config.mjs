ce7ccd3300d7e538732cc8d546c94b295abb105a
farzal182 <farzalanan_ini@yahoo.co.id>
2025-04-10 15:22:20 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
 
 export default defineConfig({
+  site: "https://farzal.space",
   output: 'server',
   adapter: netlify({
     edgeMiddleware: true
